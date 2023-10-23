6a2b8b5a99479d125d22c76a5e5a6f2e626a7695
Jcarlos1020 <mprojetos14@gmail.com>
2023-10-23 17:57:50 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,67 @@
+const button = document.querySelector(".botão-itens")
+const input = document.querySelector(".input-itens")
+const listaCompleta = document.querySelector(".lista-itens")
+
+let MinhaListaDeItens = []
+
+
+function addNovaTarefa() {
+    MinhaListaDeItens.push({ 
+        tarefa: input.value,
+        concluida: false
+})
+
+    input.value = ""
+
+    mostrarTarefas()
+}
+
+
+
+
+
+function mostrarTarefas() {
+
+    let novaLi = ""
+
+    MinhaListaDeItens.forEach((item, index) => {
+        novaLi = novaLi + `
+        
+        <li class="itens ${item.concluida && "done"}">
+    <img  src="./img/checked.png" alt="checked" onclick="concluirTarefas(${index})">
+    <p> ${item.tarefa}</p>
+    <img src="./img/trash.png" alt="lixo" onclick="deletarItens(${index})" >
+         </li>`
+    })
+
+    listaCompleta.innerHTML = novaLi
+
+    localStorage.setItem("lista", JSON.stringify(MinhaListaDeItens))
+
+}
+
+function concluirTarefas(index) {
+    MinhaListaDeItens[index].concluida = ! MinhaListaDeItens[index].concluida
+    mostrarTarefas()
+}
+
+
+
+function deletarItens(index) {
+    MinhaListaDeItens.splice(index, 1)
+    mostrarTarefas()
+}
+
+function recarregarTarefas() {
+    const tarefasLocalStorange = localStorage.getItem("lista")
+
+    if(tarefasLocalStorange) {
+        MinhaListaDeItens = JSON.parse(tarefasLocalStorange) 
+    }
+   
+    mostrarTarefas()
+}
+
+recarregarTarefas()
+
+button.addEventListener("click", addNovaTarefa)
